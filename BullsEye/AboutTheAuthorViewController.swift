38a318fcdb1474338574914695f6c6daf07b420f
Mihail Donevski <mihaildonevski@gmail.com>
2019-02-20 22:29:00 +0100

    Add first draft game

---
+++ BullsEye/AboutTheAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutTheAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Mihail Donevski on 2/11/19.
+//  Copyright © 2019 Mihail Donevski. All rights reserved.
+//
+
+import UIKit
+
+class AboutTheAuthorViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    }
+  
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
